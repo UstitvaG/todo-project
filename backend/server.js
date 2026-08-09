1648bdb8cbd3ddb6cb041203905ef5f0f22b2f4c
Ustitva G <astitv87@gmail.com>
2026-08-09 15:29:00 +0530

    prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -69,6 +69,7 @@ app.delete("/tasks/:id", (req, res) => {
 
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
